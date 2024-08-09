b8d29d9da721fd957c6ae919881a81895bb490c9
gomzyyy <158289133+gomzyyy@users.noreply.github.com>
2024-08-09 23:41:14 +0530

    UI building - right side of home...

--- client-side/portfolio/src/components/home/context-home.jsx
+++ client-side/portfolio/src/components/home/context-home.jsx
@@ -5,31 +5,46 @@ import { NAME, ROLE, BREIF_TEXT, DISCRIPTION, SKILL_LABLE } from "../../assets/d
 import { Link } from "react-router-dom"
 
 export const Left = () => {
- 
-    return(
+
+    return (
         <div className="left">
-        <div className="left-overview"><div className="profile-image">
-            <span className="hover-box name">{NAME}</span>
-            <img src="./me.png" alt="" />
+            <div className="left-overview"><div className="profile-image">
+                <span className="hover-box name">{NAME}</span>
+                <img src="./me.png" alt="" />
+            </div>
+                <div className="info">
+                    <span className="info-name">{NAME},</span><span className="info-text"> I'm a </span><span className="info-role">{ROLE}</span>
+                    <span className="info-small">{BREIF_TEXT}</span>
+                </div></div>
+            <div className="info-discription">
+                <p>{DISCRIPTION}</p>
+            </div>
+            <span className="contact-btn"><Link className="nav-option" to="/contact">Contact</Link></span>
         </div>
-            <div className="info">
-                <span className="info-name">{NAME},</span><span className="info-text"> I'm a </span><span className="info-role">{ROLE}</span>
-                <span className="info-small">{BREIF_TEXT}</span>
-            </div></div>
-        <div className="info-discription">
-            <p>{DISCRIPTION}</p>
-        </div>
-        <span className="contact-btn"><Link className="nav-option" to="/contact">Contact</Link></span>
-    </div>
     )
 };
 
-export const Right = () =>{
+export const Right = () => {
 
-    return(
+    return (
         <div className="right">
             <div className="skills-overview">
                 <div className="skill-lable">{SKILL_LABLE}</div>
+                <div className="skills-logo">
+                <span className="skill col-html">HTML5</span>
+                <span className="skill col-css">CSS3</span>
+                <span className="skill col-js">JavaScript</span>
+                <span className="skill col-react">ReactJs</span>
+                <span className="skill col-redux">Redux</span>
+                <span className="skill col-node">NodeJs</span>
+                <span className="skill col-express">ExpressJs</span>
+                <span className="skill col-mongo">MongoDB</span>
+                <span className="skill col-py">Python</span>
+                <span className="skill col-tailwind">Tailwind</span>
+                <span className="skill col-bootstrap">Bootstrap</span>
+                <span className="skill col-git">Git</span>
+                <span className="skill col-github">GitHub</span>
+                </div>
             </div>
         </div>
     )
